2eaf60c4cc1c45ab16fc89a7572ff8b87b625eab
Aisha <notaisha20@gmail.com>
2024-10-16 09:32:56 +0800

    Update and rename NewLive.tsx to new-live.tsx

--- pages/live/new-live.tsx
+++ pages/live/new-live.tsx
@@ -3,15 +3,20 @@ import React, { useState } from 'react';
 import Image from 'next/image';
 import Checkbox from '@mui/material/Checkbox';
 import { orange } from '@mui/material/colors';
-import SubmitButton from '../../src/api/components/SubmitButton';
-import CancelButton from '../../src/api/components/CancelButton';
+import SubmitButton from '../SubmitButton';
+import CancelButton from '../CancelButton';
 import SelectProductPopup from '../PopUp/SelectProductPopup';
+import '../../app/globals.css'
+import { useRouter } from 'next/navigation';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+      
 
 const NewLive = () => {
   
+  const router = useRouter();
   const [title, setTitle] = useState('');
   const [link, setLink] = useState('');
-  const [schedule, setSchedule] = useState('');
+ 
   const [comment, setComment] = useState('');
   const [code, setCode] = useState('');
   const [desc, setDesc] = useState('');
@@ -19,7 +24,9 @@ const NewLive = () => {
   const [isPopupOpen, setIsPopupOpen] = useState(false);
   const [productGroups, setProductGroups] = useState<any[]>([]);
   const [selectedTypes, setSelectedTypes] = useState<{ [key: number]: string }>({});
-
+  const [selectedIndex, setSelectedIndex] = useState(-1); 
+  const [scheduleDate, setScheduleDate] = useState('');
+  const [scheduleTime, setScheduleTime] = useState('');
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
     // Add your submission logic here
@@ -30,19 +37,28 @@ const NewLive = () => {
   const handleSelectProducts = (selectedItems: any[]) => {
     setProductGroups([...productGroups, selectedItems]);
   };
+
+  const handleCheckboxChange = (index: React.SetStateAction<number>) => {
+    // If the clicked checkbox is already selected, unselect it
+    if (selectedIndex === index) {
+      setSelectedIndex(-1);
+    } else {
+      setSelectedIndex(index); // Set the selected checkbox index
+    }
+  };
+
   return (
     <div className="w-full overflow-x-hidden mb-8">
+      
 {/* ---------------------------Header Section------------------------- */}
-<div className="w-full p-2 bg-custom-bg">
+<div className="w-full p-2 bg-custom-bg fixed top-0 z-50">
     <ul className='flex items-center h-full'>
-      <li className="flex items-center px-2">
-            <Image
-              src="/live.png"
-              alt="live"
-              width={50}
-              height={50}
-              className="object-contain bg-custom-bg"
-            />
+      <li className="flex items-center px-4">
+      <button>
+        <ArrowBackIosIcon
+        onClick={() => router.push('/live')}
+          className="object-contain text-header-bg my-4"
+        /></button>
       </li>
       <li className="flex items-center">
         <h1 className='text-header-bg text-xl md:text-2xl px-2 font-sfPro font-medium'>Live</h1> 
@@ -52,10 +68,10 @@ const NewLive = () => {
 {/* ---------------------------Header Section------------------------- */}
 
 {/* -----------------------------Breadcrumbs-------------------------- */}
-<div className="w-full p-4 border-b flex items-start">
+<div className="w-full p-4 border-b flex items-start mt-[70px]">
     <ul className="text-[1rem] font-sfPro font-medium text-gray-500"> {/* Increased font size */}
       <li className="flex items-center px-2">
-        <span className="mr-2">Live</span>
+        <span className="mr-2 underline cursor-pointer" onClick={() => router.push('/live')} >Live</span>
         <span className="mx-2">/</span>
         <span className="ml-2 text-header-bg">New</span>
       </li>
@@ -85,19 +101,33 @@ const NewLive = () => {
 {/* -------------------------------Schedule----------------------------*/}
 <div className='mb-6'>
       <label className="block text-[1rem] text-gray-500 font-sfPro font-semibold">
-          Schedule
-        </label>
-        <input
-          type="date"
-          value={schedule}
-          onChange={(e) => setSchedule(e.target.value)}
-          className='mt-2 border border-gray-300 rounded-lg 
-          p-4 w-full text-[1rem] 
-          font-sfPro font-normal 
-          placeholder-gray-300 
-          focus:border-header-bg focus:outline-none'
-        />
-</div>
+        Schedule
+      </label>
+
+      {/* Date Input */}
+      <input
+        type="date"
+        value={scheduleDate}
+        onChange={(e) => setScheduleDate(e.target.value)}
+        className='mt-2 border border-gray-300 rounded-lg 
+        p-4 w-full text-[1rem] 
+        font-sfPro font-normal 
+        placeholder-gray-300 
+        focus:border-header-bg focus:outline-none'
+      />
+
+      {/* Time Input */}
+      <input
+        type="time"
+        value={scheduleTime}
+        onChange={(e) => setScheduleTime(e.target.value)}
+        className='mt-2 border border-gray-300 rounded-lg 
+        p-4 w-full text-[1rem] 
+        font-sfPro font-normal 
+        placeholder-gray-300 
+        focus:border-header-bg focus:outline-none'
+      />
+    </div>
 {/* -------------------------------Schedule----------------------------*/}
 
 {/* -------------------------------Live URL--------------------------- */}
@@ -190,65 +220,66 @@ font-sfPro font-semibold mt-4 '>
 </button>
 {/* -----------------------Add Product Group------------------------- */}
 {productGroups.length > 0 && (
-        <div>
-          {productGroups.map((group, index) => (
-            <div key={index} className="mb-4 relative ">
-              {/*---------------- Delete button in top-right---------------- */}
-        
-{/*---------------- Delete button in top-right---------------- */}
-<table className="bg-gray-50 rounded-lg min-w-full  ">
-  <thead>
-    <tr className="text-[1rem] text-gray-500 font-semibold border-b
-    pt-4">
-    <th className="text-left px-4 py-2">Products</th>
-      <th className="text-right px-4  py-2">Price/Stocks</th>
-      <th className="text-right px-4 py-2">Pinned</th>
+  <div className='px-4'>
+    {productGroups.map((group, index) => (
+      <div key={index} className="mb-4 relative">
+        {/* Delete button in top-right */}
+        <button
+          onClick={() => {
+            setProductGroups(prev => prev.filter((_, i) => i !== index));
+            setSelectedTypes(prev => {
+              const newState = { ...prev };
+              delete newState[index];
+              return newState;
+            });
+          }}
+          className="absolute right-2 text-gray-500 text-2xl "
+        >
+          &times;
+        </button>
+        {/* Product Table */}
+        <table className="bg-gray-50 text-[1rem] rounded-lg w-full  ">
+        <thead className='border-b border-gray-300 ' >
+        <tr className="text-[1rem] text-gray-500 font-semibold border-b
+   ">
+    <th className="text-left pb-2 pt-6 px-4 ">Products</th>
+   
+    <th className="text-left pb-2 pt-6 px-4  ">Pinned</th> 
       
     </tr>
-  </thead>
-  <tbody>
-    
-  {group.map((item: any) => (
-    <tr key={item.name}>
-      <td className="flex items-start 
-      font-sfPro font-normal 
-      text-gray-500 
-      px-4 py-4">{item.name}</td>
-      <td className="font-sfPro 
-      text-gray-500 px-4 text-right py-4">
-          {item.price && item.discountedPrice ? (<>
-            
-            <span className="text-gray-500 ">{item.price}</span><br/>
-                
-                Stocks: {item.stocks}
-              </>
-            ) : (
-              `${item.product} Products`
-            )}
-          </td>
-          
-<div className='flex justify-center'>
-  <Checkbox
-    {...label}
-    defaultChecked
-    sx={{
-      color: orange[800],
-      "&.Mui-checked": {
-        color: orange[600],
-      },
-      "& .MuiSvgIcon-root": {
-        fontSize: 25, // Adjust the size of the checkbox
-      },
-    }}
-  />
-</div>
+          </thead>
+          <tbody>
+            {group.map((item: any, itemIndex: number) => (
+              <tr key={item.title}>
+                <td className="flex items-start font-sfPro font-normal text-gray-500 px-4 py-4">
+                  {item.title}
+                </td>
+                <td>
+                  <Checkbox
+                    checked={selectedIndex === itemIndex}
+                    onChange={() => handleCheckboxChange(itemIndex)}
+                    sx={{
+                      color: orange[800],
+                      "&.Mui-checked": {
+                        color: orange[600],
+                      },
+                      "& .MuiSvgIcon-root": {
+                        fontSize: 25,
+                      },
+                    }}
+                    className='flex justify-center'
+                  />
+                </td>
+                </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    ))}
+  </div>
+)}
 
-    </tr>
-  ))}
-  </tbody>
-  </table>
-     
-  </div>))}</div>)}
+
 {/* Popup Component */}
 <SelectProductPopup
         isOpen={isPopupOpen}
